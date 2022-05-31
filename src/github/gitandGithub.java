c4b993155935f779d24c642cbf91be177e131e8c
stave031 <stevealba7382@gmail.com>
2022-05-31 19:56:10 -0400

    2nd commit

--- src/github/gitandGithub.java
+++ src/github/gitandGithub.java
@@ -3,8 +3,54 @@ package github;
 public class gitandGithub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	
+			System.out.println("today we learn github");
+			System.out.println("today is a very good day");
+	
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
